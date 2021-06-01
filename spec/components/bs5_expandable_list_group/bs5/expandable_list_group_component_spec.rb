ae58d638691dced1c6059657bdddc59d9e5d5a82
Patrick Baselier <patrick.baselier@gmail.com>
2021-06-01 13:40:26 +0200

    Add travis config

--- spec/components/bs5_expandable_list_group/bs5/expandable_list_group_component_spec.rb
+++ spec/components/bs5_expandable_list_group/bs5/expandable_list_group_component_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Bs5::ExpandableListGroupComponent, type: :component do
-  it 'renders a component' do
-    render_inline(described_class.new)
-
-    expect(rendered_component).to have_css('div.list-group')
+  subject do
+    render_inline(described_class.new(options))
   end
 
-  it 'renders a component with classname' do
-    render_inline(described_class.new(class: 'lorem'))
+  describe 'simple call' do
+    let(:options) { {} }
 
-    expect(rendered_component).to have_css('div.list-group.lorem')
+    it { is_expected.to have_css('div.list-group') }
+  end
+
+  describe 'with a classname' do
+    let(:options) { { class: 'lorem' } }
+
+    it { is_expected.to have_css('div.list-group.lorem') }
   end
 end
